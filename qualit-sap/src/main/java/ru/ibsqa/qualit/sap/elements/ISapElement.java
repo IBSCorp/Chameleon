948e3654508e607081d2baeb0b3f9fafc8bc8977
Nikolay Dildin <dildin@yandex.ru>
2023-06-29 18:00:21 +0300

    Delete qualit-sap directory

--- qualit-sap/src/main/java/ru/ibsqa/qualit/sap/elements/ISapElement.java
+++
@@ -1,9 +0,0 @@
-package ru.ibsqa.qualit.sap.elements;
-
-import ru.ibsqa.qualit.elements.selenium.IFacadeSelenium;
-import com.jacob.com.Variant;
-
-public interface ISapElement extends IFacadeSelenium {
-    Variant getVariant();
-
-}
--- qualit-sap/src/main/java/ru/ibsqa/qualit/sap/search_context/SapSearchContext.java
+++
@@ -1,8 +0,0 @@
-package ru.ibsqa.qualit.sap.search_context;
-
-import ru.ibsqa.qualit.sap.driver.SapDriver;
-import org.openqa.selenium.SearchContext;
-
-public interface SapSearchContext extends SearchContext {
-    SapDriver getSapDriver();
-}
